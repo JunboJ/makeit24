43771f6d01b6ad5a56df7e0f7e7c22010177eadf
JunboJ <junboz598@gmail.com>
2021-03-03 15:20:47 +1300

    update file name

--- navigation/mainNavigator.js
+++ navigation/mainNavigator.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import GameScreen from '../Screens/GameScreen';
-import HintListScreen from '../Screens/HintListScreen';
-import MainMenuScreen from '../Screens/MainMenuScreen';
+import GameScreen from '../screens/GameScreen';
+import HintListScreen from '../screens/HintListScreen';
+import MainMenuScreen from '../screens/MainMenuScreen';
 
 const MainNavigator = createStackNavigator(
     {
@@ -12,7 +12,7 @@ const MainNavigator = createStackNavigator(
         Game: GameScreen,
         HintList: HintListScreen
     },
-    {  
+    {
         headerMode: 'float',
         defaultNavigationOptions: {
             headerTransparent: true
